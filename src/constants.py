86380128899ee597eca9bfdd74f7a75f335b53bb
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-11-24 07:45:06 +0800

    reupdated constants

--- src/constants.py
+++ src/constants.py
@@ -2,18 +2,15 @@ import numpy as np
 
 five_by_five_matrix = np.array([
     # Senior,     Mid,        Junior,     QA
-    [3.0,         2.0,        1.0,        1.0],   # Complex Story Points
+    [6.0,         3.0,        1.0,        1.0],   # Complex Story Points
     [2.0,         2.0,        1.0,        1.0],   # Infra Story Points
-    [1.0,         1.0,        1.0,        3.0],   # Maintenance Story Points
-    [1.0,         1.0,        0.0,        3.0],   # Testing Story Points
+    [2.0,         1.0,        1.0,        3.0],   # Maintenance Story Points
+    [0.0,         1.0,        0.0,        3.0],   # Testing Story Points
 ])
 
 sp_total = np.array([
+    100,
     60,
-    50,
+    70,
     40,
-    30,
 ])
-
-SPRINT_DAYS = 10
-HRS_PER_DAY = 8
