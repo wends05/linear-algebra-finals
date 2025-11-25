af14b7640a92ca3101f8f9e5ea3b1e2b0164edb8
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-11-25 14:58:53 +0800

    updated prints

--- src/test.py
+++ src/test.py
@@ -8,9 +8,9 @@ from matrix import Matrix
 if __name__ == "__main__":
     res = Matrix.gaussian_elimination(five_by_five_matrix, sp_total)
     print(f"\nTotal Hours Required: {Matrix.total_hours(res):.2f} hours")
-    print("\nMatrix Representation:")
+    print("\nMatrix Representation (A):")
     Matrix.print_matrix(five_by_five_matrix)
     production = Matrix.calculate_production(five_by_five_matrix, res)
-    print("\nActual Production (Story Points):")
+    print("\nStory Points (B):")
     for i, p in enumerate(production):
-        print(f"Category {i + 1}: {p:.2f} story points")
+        print(f"Category {i + 1}: {p:} story points")
